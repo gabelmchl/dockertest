3ac5f0807a3f4a3be08820a0580a98aaef74e6f1
gabelmchl <michael.gabel@five1.de>
2022-01-10 08:17:42 +0100

    Added init files

---
+++ python/app.py
@@ -0,0 +1,19 @@
+# import main Flask class, request object
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+# create the Flask app
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    number = request.args.get('number')
+
+    return str(2 * int(number))
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
